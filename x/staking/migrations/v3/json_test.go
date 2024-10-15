8d6dde5d60b5102e2232869adae2849bd43c9d97
ezreal1997 <gezerui1997@gmail.com>
2024-10-15 10:13:35 -0700

    feat(staking): default params

--- x/staking/migrations/v3/json_test.go
+++ x/staking/migrations/v3/json_test.go
@@ -60,12 +60,17 @@ func TestMigrateJSON(t *testing.T) {
 			"1": {
 				"duration": "7776000s",
 				"period_type": "THREE_MONTHS",
-				"rewards_multiplier": "1.100000000000000000"
+				"rewards_multiplier": "1.051000000000000000"
 			},
 			"2": {
 				"duration": "31536000s",
 				"period_type": "ONE_YEAR",
-				"rewards_multiplier": "1.200000000000000000"
+				"rewards_multiplier": "1.160000000000000000"
+			},
+			"3": {
+				"duration": "46656000s",
+				"period_type": "EIGHTEEN_MONTHS",
+				"rewards_multiplier": "1.340000000000000000"
 			}
 		}
 	},
@@ -73,11 +78,11 @@ func TestMigrateJSON(t *testing.T) {
 	"token_types": {
 		"token_type_info_map": {
 			"0": {
-				"rewards_multiplier": "1.000000000000000000",
+				"rewards_multiplier": "0.500000000000000000",
 				"token_type": "LOCKED"
 			},
 			"1": {
-				"rewards_multiplier": "2.000000000000000000",
+				"rewards_multiplier": "1.000000000000000000",
 				"token_type": "UNLOCKED"
 			}
 		}
